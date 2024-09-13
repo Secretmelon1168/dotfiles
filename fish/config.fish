f7ad58443c56f3546dbf29cff950d82311911aac
Jerry Tan <melon11681@gmail.com>
2024-09-13 18:56:36 +1200

    Update configs

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,7 @@ if status is-interactive
 
 	# ZSH alias copy and pasting
 	alias mountwin='sudo mount /dev/nvme0n1p3 /run/mount'
-	alias sd0="shutdown 0"
-	alias rb0="systemctl reboot"
-	alias dslr_capture="gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0"
+	alias dslr_capture="gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video1"
 	alias hdmion="xrandr --output 'eDP1' --auto --output 'HDMI1' --auto --right-of 'eDP1' | wal -R"
 	alias hdmioff="xrandr --output 'eDP1' --auto --output 'HDMI1' --off"
 	alias walr="wal -R"
@@ -15,7 +13,8 @@ if status is-interactive
 	alias pywal-obsidian="pkill obsidian & /home/melon/pywal-obsidianmd/pywal-obsidianmd.sh "/home/melon/obsidian/Uni""
 	alias iwdwifion="iwctl device wlan0 set-property Powered on"
 	alias iwdwifioff="iwctl device wlan0 set-property Powered off"
-	alias sudo!!="sudo $history[1]"
+	alias l="ls -l"
+	alias vi='vim'
 	cat ~/.cache/wal/sequences
 	meowfetch
 end
